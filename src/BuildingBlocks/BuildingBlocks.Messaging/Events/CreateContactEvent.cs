6ca5fa2cc79d38a2b2eff915696f6dfd3ef0ac2a
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-04 17:07:29 -0300

    feat: add contact integration event

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/CreateContactEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record CreateContactEvent : IntegrationEvent
+{
+    public string Name { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string? Email { get; set; }
+}
