77b4a7a6fc99834126b8b5a88b1262a7660ac88e
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-05 18:35:35 -0300

    feat - contact persistency ms: check unique contact endpoint

---
+++ src/Services/ContactPersistency/ContactPersistence.Application/Contacts/Queries/CheckUniqueContact/CheckUniqueContactHandler.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.CQRS;
+using ContactPersistence.Application.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactPersistence.Application.Contacts.Queries.CheckUniqueContact;
+
+public class CheckUniqueContactHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<CheckUniqueContactQuery, CheckUniqueContactResult>
+{
+    public async Task<CheckUniqueContactResult> Handle(CheckUniqueContactQuery query, CancellationToken cancellationToken)
+    {
+        var contactExists = await dbContext.Contacts.AnyAsync(c => c.Region.DddCode == query.DddCode
+                            && c.Phone == query.Phone, cancellationToken);
+
+        var emailExists = await dbContext.Contacts.AnyAsync(c => c.Email == query.Email, cancellationToken);
+
+        return new CheckUniqueContactResult(!(emailExists || contactExists));
+    }
+
+}
+
---
+++ src/Services/ContactPersistency/ContactPersistence.Application/Contacts/Queries/CheckUniqueContact/CheckUniqueContactQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace ContactPersistence.Application.Contacts.Queries.CheckUniqueContact;
+
+public record CheckUniqueContactQuery(string? Email, int? DddCode, string? Phone)
+    : IQuery<CheckUniqueContactResult>;
+
+public record CheckUniqueContactResult(bool isUnique);
