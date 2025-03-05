f5365e9ea4aa66df205851779319701dbc2c738d
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-05 12:27:56 -0300

    feat - contact persistency ms: domain layer

---
+++ src/Services/ContactPersistency/ContactPersistence.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactPersistence.Domain.Common;
+
+public abstract class BaseDomainEntity()
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedBy { get; init; }
+    public DateTime ModifiedAt { get; set; }
+    public Guid ModifiedBy { get; set; }
+}
