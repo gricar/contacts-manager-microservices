88f31c2a58a2072ee0b18c4b9b0b3cf98b158df7
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-05 18:35:13 -0300

    feat - contact persistency ms: save to DB

--- src/Services/ContactPersistency/ContactPersistence.Application/Contacts/Commands/CreateContact/CreateContactHandler.cs
+++ src/Services/ContactPersistency/ContactPersistence.Application/Contacts/Commands/CreateContact/CreateContactHandler.cs
@@ -1,22 +1,26 @@
 ﻿using BuildingBlocks.CQRS;
+using ContactPersistence.Application.Data;
 using ContactPersistence.Application.DTOs;
 using ContactPersistence.Domain.Models;
 
 namespace ContactPersistence.Application.Contacts.Commands.CreateContact;
 
-public class CreateContactHandler : ICommandHandler<CreateContactCommand, CreateContactResult>
+public class CreateContactHandler(IApplicationDbContext dbContext)
+    : ICommandHandler<CreateContactCommand, CreateContactResult>
 {
     public async Task<CreateContactResult> Handle(CreateContactCommand command, CancellationToken cancellationToken)
     {
         var contact = CreateNewContact(command.Contact);
 
         //store in DB
+        await dbContext.Contacts.AddAsync(contact, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
 
         return new CreateContactResult(contact.Id);
     }
 
     private Contact CreateNewContact(ContactDto contact)
     {
-        return Contact.Create(contact.Name, 11, contact.Phone, contact.Email);
+        return Contact.Create(contact.Name, contact.DDDCode, contact.Phone, contact.Email);
     }
 }
