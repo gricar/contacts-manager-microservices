4478364dc5d7d5151feb9be73082d511df03ce69
Gabriel Ricardo <gabriel.rodrigues.ricardo@outlook.com>
2025-03-17 19:31:21 -0300

    refactor: create contact

--- src/Services/ContactPersistency/ContactPersistence.Application/Contacts/Commands/CreateContact/CreateContactHandler.cs
+++ src/Services/ContactPersistency/ContactPersistence.Application/Contacts/Commands/CreateContact/CreateContactHandler.cs
@@ -1,26 +1,23 @@
 ﻿using BuildingBlocks.CQRS;
 using ContactPersistence.Application.Data;
-using ContactPersistence.Application.DTOs;
 using ContactPersistence.Domain.Models;
+using Microsoft.Extensions.Logging;
 
 namespace ContactPersistence.Application.Contacts.Commands.CreateContact;
 
-public class CreateContactHandler(IApplicationDbContext dbContext)
+public class CreateContactHandler(IApplicationDbContext dbContext, ILogger<CreateContactHandler> logger)
     : ICommandHandler<CreateContactCommand, CreateContactResult>
 {
     public async Task<CreateContactResult> Handle(CreateContactCommand command, CancellationToken cancellationToken)
     {
-        var contact = CreateNewContact(command.Contact);
+        var contact = Contact.Create(command.Name, command.DDDCode, command.Phone, command.Email);
 
         //store in DB
         await dbContext.Contacts.AddAsync(contact, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
 
-        return new CreateContactResult(contact.Id);
-    }
+        logger.LogInformation("Contact saved in database: {ContactId}", contact.Id);
 
-    private Contact CreateNewContact(ContactDto contact)
-    {
-        return Contact.Create(contact.Name, contact.DDDCode, contact.Phone, contact.Email);
+        return new CreateContactResult(contact.Id);
     }
 }
--- src/Services/ContactPersistency/ContactPersistency.API/Endpoints/CreateContact.cs
+++ src/Services/ContactPersistency/ContactPersistency.API/Endpoints/CreateContact.cs
@@ -1,31 +1,21 @@
 ﻿using Carter;
 using ContactPersistence.Application.Contacts.Commands.CreateContact;
-using ContactPersistence.Application.DTOs;
-using Mapster;
 using MediatR;
 
 namespace ContactPersistency.API.Endpoints;
-
-public record CreateContactRequest(ContactDto Contact);
-
-public record CreateContactResponse(Guid Id);
 
 public class CreateContact : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/contacts", async (CreateContactRequest request, ISender sender) =>
+        app.MapPost("/contacts", async (CreateContactCommand command, ISender sender) =>
         {
-            var command = request.Adapt<CreateContactCommand>();
-
             var result = await sender.Send(command);
 
-            var response = result.Adapt<CreateContactResponse>();
-
-            return Results.Created($"/contacts/{response.Id}", response);
+            return Results.Created($"/contacts/{result.Id}", result);
         })
         .WithName("CreateContact")
-        .Produces<CreateContactResponse>(StatusCodes.Status201Created)
+        .Produces<CreateContactResult>(StatusCodes.Status201Created)
         .ProducesProblem(StatusCodes.Status400BadRequest)
         .WithSummary("Create Contact")
         .WithDescription("Create Contact");
